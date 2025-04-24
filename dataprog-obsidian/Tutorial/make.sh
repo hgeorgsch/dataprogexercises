4f5a82739808ed6b573db7271f077a49d48c7dab
Hans Georg Schaathun <georg@schaathun.net>
2025-04-24 17:28:54 +0200

    comments

--- dataprog-obsidian/Tutorial/make.sh
+++ dataprog-obsidian/Tutorial/make.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+# This is to build the Jupyter Book, including generating notebook files
+# from markdown.
 
 ( cd notebook ; for i in *.md ; do jupytext --to notebook "$i" ; done )
 jupyter-book build .
