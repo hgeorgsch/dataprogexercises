4880ac39ea22abb7fb5eedecf11d4016c23de2e5
Hans Georg Schaathun <georg@schaathun.net>
2025-05-22 05:47:48 +0200

    Established build dir - not tested

--- book/make.sh
+++ book/make.sh
@@ -2,7 +2,17 @@
 # This is to build the Jupyter Book, including generating notebook files
 # from markdown.
 
-( cd notebook ; for i in *.md ; do jupytext --to notebook "$i" ; done )
+T=../dataprog-obdsidian/Tutorial/
+N=$T/notebook
+
+( cd $N ; for i in *.md ; do jupytext --to notebook "$i" ; done )
+
+mkdir -p notebook
+cp $N/*.{csv,json,ipynb notebook 
+
+cp $T/*.md .
+
+
 jupyter-book build .
 
 rsync -av _build/html/ hasc@login.ansatt.ntnu.no:/home/groupswww/iirevu/evu2025/
