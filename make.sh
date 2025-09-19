8d7dcbc299f07d0d333bdec622493860f60447b5
Hans Georg Schaathun <georg@schaathun.net>
2025-09-19 07:54:53 +0200

    copied build scripts as stubs

---
+++ make.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# This is to build the Jupyter Book, including generating notebook files
+# from markdown.
+
+T=../dataprog-obsidian/Tutorial/
+NS="notebook Genetikk Jordskjelv"
+ls $T
+
+for N1 in $NS
+do
+   N=${T}/${N1}
+   ( cd $N ; for i in *.md ; do jupytext --to notebook "$i" ; done )
+   mkdir -p $N1
+
+   cp $N/*.csv $N1 
+   cp $N/*.txt $N1 
+   cp $N/*.json $N1 
+   cp $N/*.ipynb $N1 
+   cp $N/*.jpg $N1 
+done
+
+
+cp $T/*.md .
+
+
+jupyter-book build .
+
+rsync -av _build/html/ hasc@login.ansatt.ntnu.no:/home/groupswww/iirevu/iira2001/
