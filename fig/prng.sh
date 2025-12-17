b8053e04eaa36c0b7e1ecaee13b2a99c67c69400
Hans Georg Schaathun <georg@schaathun.net>
2025-12-17 13:58:31 +0100

    Generated animation figures

--- fig/prng.sh
+++ fig/prng.sh
@@ -3,9 +3,10 @@ args="{7}{49}{0.02} {49}{19}{0.052} {19}{52}{0.0192} {52}{40}{0.025} {40}{37}{0.
 
 c=0
 
-for i in args
+for i in $args
 do
    c=`expr $c + 1`
-   sed -e "s/%ARG%/$i" prngx.tex > prng$c.tex
+   sed -e "s/%ARG%/$i/" prngx.tex > prng$c.tex
    pdflatex prng$c
+   pdf2svg prng$c.pdf prng$c.svg
 done
