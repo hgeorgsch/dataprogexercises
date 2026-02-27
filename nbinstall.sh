11d70c01153434f5c75256fdd98889fd481cf868
Hans Georg Schaathun <georg@schaathun.net>
2026-02-27 21:56:38 +0100

    Fixed nbinstall to take multiple arguments + bugfix

--- nbinstall.sh
+++ nbinstall.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
 D="iira6001/notebooks/"
-T=$1
 
-for i in $T/*.md 
+for T 
 do
-   jupytext --to notebook --execute "$i" 
-done
-cd $R
 
-cp $T/*.csv $D
-cp $T/*.txt $D
-cp $T/*.json $D
-cp $T/*.ipynb $D
-cp $T/*.jpg $D
-cp $T/*.svg $D
-cp $T/*.png $D
+  for i in $T/*.md 
+  do
+    jupytext --to notebook --execute "$i" 
+  done
+
+  cp $T/*.csv $D
+  cp $T/*.txt $D
+  cp $T/*.json $D
+  cp $T/*.ipynb $D
+  cp $T/*.jpg $D
+  cp $T/*.svg $D
+  cp $T/*.png $D
+done
